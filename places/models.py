a2883bb241e0db659b8ca0be5befd9a3211b06b6
lau briel <ada_hack23@hackwomen.dev>
2023-03-24 20:06:48 -0600

    add models and serializers

--- places/models.py
+++ places/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+
+    name =models.CharField (max_length=56)
+    description = models.CharField (max_length=256)
+    address_state= models.CharField (max_length=32)
+    address_city = models.CharField( max_length=32)
+    address_colonia = models.CharField(max_length=32)
+    address_street = models.CharField(max_length= 32)
+    address_zipcode = models.CharField(max_length= 32)
+
+class Meta:
+    db_table = 'places'
+
+def _str_ (self):
+    return self.name
