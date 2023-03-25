b1ab9d6710cbec83a1d8e238de75cc9e77af4cf4
Miriam Zamora Morales <ada_hack410@hackademy.mx>
2023-03-25 00:56:33 -0600

    fix env vars

--- lugares_seguros/wsgi.py
+++ lugares_seguros/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lugares_seguros.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lugares_seguros.settings.base')
 
 application = get_wsgi_application()
