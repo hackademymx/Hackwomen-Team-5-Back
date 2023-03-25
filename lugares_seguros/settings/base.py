2c958f2add81fd62ecba800eed981f52b65b1be5
miri9810 <ada_hack410@hackademy.mx>
2023-03-24 21:30:33 -0600

    settings

--- lugares_seguros/settings/base.py
+++ lugares_seguros/settings/base.py
@@ -94,10 +94,20 @@ WSGI_APPLICATION = 'lugares_seguros.wsgi.application'
 
 
 # DATABASES
+PG_DB = config('POSTGRES_DB', default='')
+PG_USER = config('POSTGRES_USER', default='')
+PG_PASSWD = config('POSTGRES_PASSWORD', default='')
+PG_HOST = config('POSTGRES_HOST', default='127.0.0.1')
+PG_PORT = config('POSTGRES_PORT', cast=int, default=5432)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PG_DB,
+        'USER': PG_USER,
+        'PASSWORD': PG_PASSWD,
+        'HOST': PG_HOST,
+        'PORT': PG_PORT,
     }
 }
 
