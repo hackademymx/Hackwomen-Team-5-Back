d25f007ad5659e4f01b51d53fa435dcd7f823be5
Lariur Alejandra Avila <avilalariur@gmail.com>
2023-04-11 23:56:04 -0300

    se agregaron views y urls

---
+++ api/places/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PlaceAPIView, PlaceAPIUpdateDeleteView
+
+
+urlpatterns = [
+    path('places/', PlaceAPIView.as_view(), name='places'),
+    path('places/<int:id>/', PlaceAPIUpdateDeleteView.as_view(), name='place-update-delete'),
+]
+
+    
--- api/places/views.py
+++ api/places/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from .serializers import PlaceSerializer
+from .models import Place
 
 # Create your views here.
+
+class PlaceAPIView(APIView):
+
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get(self, request):
+        places = Place.objects.all()
+        serializer = PlaceSerializer(places, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PlaceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class PlaceAPIUpdateDeleteView(APIView):
+
+    def patch (self, request, id):
+        place = Place.objects.filter(id=id).first()
+        if place is None:
+            return Response({'error': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = PlaceSerializer(place, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        place = Place.object.filter(id=id).first()
+        if place is None:
+            return Response({'error': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
+        place.delete()
+        return Response({'message': 'this place has been successfully removed'}, status=status.HTTP_200_OK)
+            
+
