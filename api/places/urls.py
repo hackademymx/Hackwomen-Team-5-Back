0aa8be9378e7d017d18a9bc25e89f898f18dd6d1
Lariur Alejandra Avila <avilalariur@gmail.com>
2023-04-19 00:18:02 -0300

    se corrigieron los endpoints

--- api/places/urls.py
+++ api/places/urls.py
@@ -3,8 +3,6 @@ from .views import PlaceAPIView, PlaceAPIUpdateDeleteView
 
 
 urlpatterns = [
-    path('places/', PlaceAPIView.as_view(), name='places'),
-    path('places/<int:id>/', PlaceAPIUpdateDeleteView.as_view(), name='place-update-delete'),
+    path('', PlaceAPIView.as_view(), name='places'),
+    path('<int:id>/', PlaceAPIUpdateDeleteView.as_view(), name='place-update-delete'),
 ]
-
-    
